ce93ca08ff7267734d11720dad901e0a2ce238bb
antoinedelplace <>
2024-10-03 11:00:00 +0200

    Depth estimation on folder

--- dataset_preprocessing/depth_estimation.py
+++ dataset_preprocessing/depth_estimation.py
@@ -7,11 +7,11 @@ import numpy as np
 import matplotlib
 
 from utils.video_utils import frame_gen_from_video
-from utils.general_utils import time_it
 from utils.depth_anything_v2_utils import BatchPredictor
+from utils.general_utils import try_wrapper
 
-input_path = "../../data/resized_data/df5afa6a-b7a2-485e-ae12-e3d045e4ebc0-original.mp4"
-output_folder = "../../data/detectron2_data/"
+input_folder = "../../data/resized_data/"
+output_folder = "../../data/depth_data/"
 os.makedirs(output_folder, exist_ok=True)
 log_path = os.path.join(output_folder, "error_log.txt")
 
@@ -24,6 +24,14 @@ model_configs = {
     'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
     'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
 }
+
+batch_size = 12
+workers = 8
+input_size = 768
+
+depth_anything = BatchPredictor(batch_size, workers, input_size, input_size, **model_configs[encoder])
+depth_anything.load_state_dict(torch.load(f'../../checkpoints/depth_anything_v2_{encoder}.pth', map_location='cpu'))
+depth_anything = depth_anything.to(DEVICE).eval()
 
 cmap = matplotlib.colormaps.get_cmap('Spectral_r')
 
@@ -48,13 +56,6 @@ def run_on_video(input_path):
         frameSize=(width, height),
         isColor=True,
     )
-
-    batch_size = 12
-    workers = 8
-
-    depth_anything = BatchPredictor(batch_size, workers, width, height, **model_configs[encoder])
-    depth_anything.load_state_dict(torch.load(f'../../checkpoints/depth_anything_v2_{encoder}.pth', map_location='cpu'))
-    depth_anything = depth_anything.to(DEVICE).eval()
     
     frame_gen = frame_gen_from_video(video)
 
@@ -62,26 +63,34 @@ def run_on_video(input_path):
         mini = output_batch.min()
         depth = (output_batch - mini) / (output_batch.max() - mini) * 255.0
         depth = depth.astype(np.uint8)
-        depth = (cmap(depth)[:, :, :3] * 255)[:, :, ::-1].astype(np.uint8)
+        depth = (cmap(depth)[:, :, :, :3] * 255)[:, :, :, ::-1].astype(np.uint8)
 
-        output_file.write(depth)
+        for frame in depth:
+            output_file.write(frame)
 
     video.release()
     output_file.release()
 
-    video = cv2.VideoCapture(os.path.join(output_folder, basename))
+    # video = cv2.VideoCapture(os.path.join(output_folder, basename))
 
-    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    frames_per_second = video.get(cv2.CAP_PROP_FPS)
-    num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    print("basename", basename)
-    print("width", width)
-    print("height", height)
-    print("frames_per_second", frames_per_second)
-    print("num_frames", num_frames)
+    # width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # frames_per_second = video.get(cv2.CAP_PROP_FPS)
+    # num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    # print("basename", basename)
+    # print("width", width)
+    # print("height", height)
+    # print("frames_per_second", frames_per_second)
+    # print("num_frames", num_frames)
 
-run_on_video(input_path)
+output_files = set(os.listdir(output_folder))
+
+for filename in tqdm.tqdm(os.listdir(input_folder)):
+    if filename in output_files:
+        continue
+
+    input_path = os.path.join(input_folder, filename)
+    try_wrapper(lambda: run_on_video(input_path), filename, log_path)
 
 
 # python dataset_preprocessing/depth_estimation.py
